b1f30ea9f8b1790e11b34bb73946753db0080a2c
unknown <levyrodriguez02@gmail.com>
2022-02-21 20:46:39 -0300

    ejercicios terminados

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -13,7 +13,7 @@ const nuevoBool = false;
 const nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * 4 === 40 ;
+const nuevaMultiplicacion = 10 * 4 === 40;
 
 // Resuelve el siguiente problema matemático:
 const nuevoModulo = 21 % 5 === 1;
@@ -155,25 +155,30 @@ function elevarAlCubo(num) {
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
+  var valor = Math.pow(num, exponent);
+  return valor;
   
 }
 
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
-  
+  var valor = Math.round(num);
+  return valor;  
 }
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
-  
+  var valor = Math.ceil(num);
+  return valor;  
 }
 
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-  
+  var num = console.log(Math.random());
+  return num;
 }
 
 function esPositivo(numero) {
@@ -181,39 +186,53 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  
-}
+  var valor = numero;
+  if(valor == 0){
+    return false;
+  }
+  else{ if(valor < 0){
+    console.log(valor, ' Es negativo');
+  }
+  else {(valor > 0) 
+  console.log(valor, ' Es Positivo');}}}
+
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  var valor = (str + '!');
+  console.log(valor);
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  
+  console.log(nombre+apellido);
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  
+  console.log('hola  '+ nombre + '!')  
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  
+  var area = alto*ancho;
+  return area;  
 }
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
+  var perimetro = lado*4;
+  console.log('El perimetro es:');
+  return perimetro;
   
 }
 
@@ -221,7 +240,8 @@ function retornarPerimetro(lado){
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-
+  var area = (base*altura) / 2;
+  return area;
 }
 
 
@@ -229,18 +249,46 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+  var valor = euro * 1.20;
+  console.log('la cantidad de: '+ euro + ' euros '+ ' es equivalente a: ');
+  console.log(valor + ' dolares');  
 }
 
 
 function esVocal(letra){
-  //Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”. 
-  //Verificar si el usuario ingresó un string de más de un carácter, en ese caso, informarle 
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
+  var caracteres = letra.length;
+  if ( caracteres == 1){
+    if( letra == 'a'){
+      console.log('Es vocal')}
+      else {if(letra == 'e'){
+        console.log('Es vocal')}
+        else{if( letra == 'i'){
+          console.log('Es vocal')}
+          else{ if( letra == 'o'){
+            console.log('Es vocal')}
+            else{if(letra == 'u'){
+              console.log('Es vocal')}
+            else{
+              console.log('dato incorrecto')}
+            }
+          }}   
+             }
+      }
+      else{console.log('Dato incorrecto')}}
+
+   
+      
+    
+   
+ //Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”. 
+  //Verificar si el usuario ingresó un string de más de un carácter, en ese caso, informarle 
+ 
   
-}
+  
+
 
 
 
