704c447a85847af0201234c49858f5d1ed6450c0
unknown <levyrodriguez02@gmail.com>
2022-04-06 18:12:52 -0300

    prueba de correxion js

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -148,17 +148,29 @@ function numeroMasGrande(numeros) {
 }
 
 
-function multiplicarArgumentos() {
+function multiplicarArgumentos(arguments = []) {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  if(arguments.length < 1) return 0;
+  var total = 1;
+  for(var i = 0; i < arguments.length; i++) {
+    total = total * arguments[i];
+  }
+  return total;
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+  var x = 0;
+  for(var i =0; i < arreglo.length;i++){
+    if(arreglo[i] > 18){
+      x++
+    }
+  }
+  return x;
 }
 
 
@@ -167,7 +179,12 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
+  if(numeroDeDia == 2 || numeroDeDia == 3 ||numeroDeDia == 4 ||numeroDeDia == 5 ||numeroDeDia == 6) {
+    return 'Es dia laboral';
+  }
+  else if(numeroDeDia == 1 ||numeroDeDia == 7){
+    return 'Es fin de semana';
+  }
 } 
 
 
