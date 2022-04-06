1186850a71281715ba894cbf106e5af37124df4b
unknown <levyrodriguez02@gmail.com>
2022-04-06 18:47:51 -0300

    error en test

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -152,8 +152,7 @@ function multiplicarArgumentos(arguments) {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
-  if(arguments.length < 1){ 
-    return 0;}
+  if(arguments.length < 1) return 0;
   var total = 1;
   for(var i = 0; i < arguments.length; i++) {
     total = total * arguments[i];
@@ -165,13 +164,14 @@ function multiplicarArgumentos(arguments) {
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-  var x = 0;
-  for(var i =0; i < arreglo.length;i++){
+  let contador = 0;
+  for (let i = 0; i < arreglo.length ; i++) {
     if(arreglo[i] > 19){
-      x++;
+      contador++
     }
   }
-  return x;
+  return contador
+
 }
 
 
@@ -180,12 +180,10 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-   if(numeroDeDia == 1 ||numeroDeDia == 7){
-    return 'Es fin de semana';
+  if(numeroDeDia === 1 || numeroDeDia === 7 ){
+    return "Es fin de semana"
   }
-  else{
-    return 'Es dia laboral';
-  }
+  return "Es dia Laboral"
 }
 
 
