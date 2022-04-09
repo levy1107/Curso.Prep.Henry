d233d14c699e97fed79a7a6096c4de857afe12e5
unknown <levyrodriguez02@gmail.com>
2022-04-09 19:01:03 -0300

    resolviendos jsv

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -148,7 +148,7 @@ function pasarUsuarioAPremium(usuarios ){
 function sumarLikesDeUsuario(usuario) {
   // "usuario" tiene una propiedad llamada "posts" que es un array
   // "posts" es un array de objetos "post"
-  // Cada objeto "post" tiene una propiedad llamada "likes" que es un entero (int/integer)
+  // Cada objeto "post" tienge una propiedad llamada "likes" que es un entero (int/integer)
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,12 +8,29 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
+
+
+  function Usuario(opciones){
+   this.usuario = opciones.usuario;
+    this.nombre  = opciones.nombre;
+    this.email   = opciones.email;
+  this.password= opciones.password;
+  }
+  Usuario.prototype.saludar = function(){
+    return 'Hola, mi nombre es '  + this.nombre;
+};
+    return Usuario;
+
 }
+
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
+  Constructor.prototype.saludar = function(){
+    return 'Hello World!';
+  }
 }
 
 function agregarStringInvertida() {
@@ -22,6 +39,14 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
+  String.prototype.reverse = function(){
+    var x = '';
+    for(var i = this.length - 1; i >= 0; i--){
+      x = x +this.charAt(i);
+      console.log(this.charAt(i));
+    }
+    return x;
+  }  
 }
 
 // ---------------------------------------------------------------------------//
