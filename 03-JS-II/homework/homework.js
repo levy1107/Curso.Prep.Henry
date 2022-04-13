8c133da0298c7dcbcd85a3a50a7a0cc27cca18aa
unknown <levyrodriguez02@gmail.com>
2022-04-12 21:34:44 -0300

    reparacion de vs code

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -3,7 +3,7 @@
 function obtenerMayor(x, y) {
   // "x" e "y" son números enteros (int).
   // Devuelve el número más grande
-  // Si son iguales, devuelve cualquiera de los dos
+  // Si son iguales, devulve cualquiera de los dos
   // Tu código:
   if(x == y){
     return x;}
